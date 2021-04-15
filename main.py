4c9c6a7f87062e1a9a857321d819e857f5190748
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2021-04-15 17:46:31 -0400

    Move csv files to data directory

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ Main module for MAL-groupwatch
 from load_graph import load_graph
 
 if __name__ == '__main__':
-    graph = load_graph("rating.csv", "anime.csv")
+    graph = load_graph("data/rating.csv", "data/anime.csv")
 
     ...
