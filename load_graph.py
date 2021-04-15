58369424411069e6698fec8022eaa0addb2fa69e
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2021-04-15 14:22:55 -0400

    Implement load_graph
    Also add function call in main.py.

---
+++ load_graph.py
@@ -0,0 +1,45 @@
+""""
+Module Description:
+
+"""
+from recommendations import WeightedGraph
+import csv
+
+
+def load_graph(reviews_file: str, animes_file: str) -> WeightedGraph:
+    """
+    Returns a bipartite graph corresponding to the datasets.
+
+    TODO: write documentation
+
+    Adapted from Assignment 3.
+    """
+    graph = WeightedGraph()
+    catalogue = {}
+
+    with open(animes_file) as f:
+        reader = csv.reader(f)
+        reader.next()
+
+        for row in reader:
+            anime_id = int(row[0])
+            title = row[1]
+
+            catalogue[anime_id] = title
+
+    with open(reviews_file) as f:
+        reader = csv.reader(f)
+        reader.next()
+
+        for row in reader:
+            user_id = int(row[0])
+            anime_id = int(row[1])
+            rating = int(row[2])
+
+            graph.add_vertex(user_id, 'user')
+            graph.add_vertex(catalogue[anime_id], 'anime')
+
+            if rating != -1:
+                graph.add_edge(user_id, catalogue[anime_id], rating)
+
+    return graph
--- main.py
+++ main.py
@@ -2,5 +2,9 @@
 Main module for MAL-groupwatch
 """
 
+from load_graph import load_graph
+
 if __name__ == '__main__':
+    graph = load_graph("rating.csv", "anime.csv")
+
     ...
