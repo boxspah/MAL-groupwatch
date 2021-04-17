974473164788496637579fb81a6aa15dfa7ed26f
Wilson Sy <10039348+boxspah@users.noreply.github.com>
2021-04-17 03:01:32 -0400

    Run PyTA on main.py

--- main.py
+++ main.py
@@ -6,11 +6,25 @@ Module Description:
 This is the main module of the MAL Groupwatch project.
 This is the module that you should run to start the program!
 """
-from PyQt5 import QtWidgets
 import sys
+
+from PyQt5 import QtWidgets
 from main_window import UiMainWindow
 
 if __name__ == '__main__':
+    # import python_ta.contracts
+    # python_ta.contracts.check_all_contracts()
+
+    import doctest
+    doctest.testmod()
+
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 100,
+        'disable': ['E1136'],
+        'extra-imports': ['PyQt5', 'sys', 'main_window'],
+    })
+
     app = QtWidgets.QApplication(sys.argv)
     main_window1 = QtWidgets.QMainWindow()
     ui = UiMainWindow()
